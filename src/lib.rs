3f0b767affc37364a11efef19cc7213b0783a153
Simone Romano <simone.romano@webcom-tlc.com>
2025-02-20 19:25:13 +0100

    Add feature flags for chat completion and audio transcription modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-pub mod audio_transcription;
-pub mod chat_completition;
 pub mod client;
+
+#[cfg(feature = "chat_completition")]
+pub mod chat_completition;
+
+#[cfg(feature = "audio_transcription")]
+pub mod audio_transcription;
+
 pub mod prelude;
 
 pub use http;
